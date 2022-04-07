dc34935f4898b67977f5dfaaec6bd25b84859a7d
ribilynn <ribilynn@gmail.com>
2022-04-07 10:24:19 +0900

    Initial Commit

---
+++ yumemi-ios-training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yumemi-ios-training
+//
+//  Created by Zhou Chang on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
