f32719821ecbff053eb8ed5ffaa20e245277e073
ribilynn <ribilynn@gmail.com>
2022-04-15 09:42:37 +0900

    Minor improvement

--- yumemi-ios-training/WeatherRequest+Client.swift
+++ yumemi-ios-training/WeatherRequest+Client.swift
@@ -11,13 +11,14 @@ final class WeatherClient {
     func fetchWeather(area: String, date: Date = Date()) throws -> Weather {
         let request = WeatherRequest(area: area, date: date)
         let requestData = try WeatherClient.encoder.encode(request)
-        if let requestString = String(data: requestData, encoding: .utf8) {
-            let reponseString = try YumemiWeather.fetchWeather(requestString)
-            if let reponseData = reponseString.data(using: .utf8) {
-                return try WeatherClient.decoder.decode(Weather.self, from: reponseData)
-            }
+        guard let requestString = String(data: requestData, encoding: .utf8) else {
+            throw YumemiWeatherError.unknownError
         }
-        throw YumemiWeatherError.unknownError
+        let reponseString = try YumemiWeather.fetchWeather(requestString)
+        guard let reponseData = reponseString.data(using: .utf8) else {
+            throw YumemiWeatherError.unknownError
+        }
+        return try WeatherClient.decoder.decode(Weather.self, from: reponseData)
     }
 }
 
